a4f2f87be7854f4021ac2ca4fc4b07c04b5bf4c1
jofjof <116435248+jofjof@users.noreply.github.com>
2024-03-06 20:51:04 +0200

    just path in server for image

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -13,16 +13,16 @@ declare global {
 }
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/')
-    },
-    filename: function (req, file, cb) {
-        const ext = file.originalname.split('.')
-            .filter(Boolean) // removes empty extensions (e.g. `filename...txt`)
-            .slice(1)
-            .join('.')
-        cb(null, Date.now() + "." + ext)
-    }
+  destination: function (req, file, cb) {
+    cb(null, 'public/')
+  },
+  filename: function (req, file, cb) {
+    const ext = file.originalname.split('.')
+      .filter(Boolean) // removes empty extensions (e.g. `filename...txt`)
+      .slice(1)
+      .join('.')
+    cb(null, Date.now() + "." + ext)
+  }
 })
 const upload = multer({ storage: storage });
 /**
@@ -55,6 +55,6 @@ const upload = multer({ storage: storage });
 *         description: 'Internal server error'
 */
 router.post('/', upload.single("file"), function (req, res) {
-    res.status(200).send({ url: process.env.BASE_URL + req.file.path })
+  res.status(200).send({ url: '/' + req.file.path })
 });
 export = router;
