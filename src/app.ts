d1547d346c61797437350b5115fe37717acbd977
jofjof <116435248+jofjof@users.noreply.github.com>
2024-03-09 19:32:22 +0200

    cors another try

--- src/app.ts
+++ src/app.ts
@@ -27,13 +27,13 @@ const initApp = (): Promise<Express> => {
       app.use(cors());
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
-      app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "*");
-        res.header("Access-Control-Allow-Headers", "*");
-        res.header("Access-Control-Allow-Credentials", "true");
-        next();
-      });
+      // app.use((req, res, next) => {
+      //   res.header("Access-Control-Allow-Origin", "*");
+      //   res.header("Access-Control-Allow-Methods", "*");
+      //   res.header("Access-Control-Allow-Headers", "*");
+      //   res.header("Access-Control-Allow-Credentials", "true");
+      //   next();
+      // });
       app.use("/post", postRouter);
       app.use("/chat", chatRouter);
       app.use("/user", userRouter);
