17c4c5385e48048fd0a2d74b4ee60296cd32eaa3
jofjof <116435248+jofjof@users.noreply.github.com>
2024-03-09 19:27:05 +0200

    cors try

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ const initApp = (): Promise<Express> => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "*");
         res.header("Access-Control-Allow-Headers", "*");
+        res.header("Access-Control-Allow-Credentials", "true");
         next();
       });
       app.use("/post", postRouter);
